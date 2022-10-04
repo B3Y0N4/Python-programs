0e388269b17075c4ceedc7674a621a564ad081d6
Abduljalal lawal <72824981+B3Y0N4@users.noreply.github.com>
2022-10-05 00:15:24 +0300

    Add files via upload

---
+++ Akıllı Sözlük Projesi/Dictionary.py
@@ -0,0 +1,37 @@
+import json
+from difflib import get_close_matches as match
+
+
+data = json.load(open("data.json"))
+
+def dictionary(word):
+    word = word.lower()
+    if(word in data):
+        return data[word]
+    elif(len(match(word, data.keys(), cutoff=0.8)) > 0):
+        answer = input("Did you mean %s ? Y/N: " % match(word, data.keys())[0])
+        if(answer=="Y"):
+            return data[match(word, data.keys())[0]]
+        elif(answer=="N"):
+            return "The word doesn't exist!"
+        else:
+            return "Error!"
+
+    else:
+        return "The word doesn't exist!"
+
+
+
+
+
+
+
+word = input("Enter word: ")
+output = dictionary(word)
+
+if(type(output)==list):
+
+    for i in output:
+        print(i)
+else:
+    print(output)
